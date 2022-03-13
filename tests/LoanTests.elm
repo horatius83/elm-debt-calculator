f62ebe5a19a0259c3a87ea5bcefdb257bb280441
horatius83 <horatius83@gmail.com>
2022-03-12 22:58:42 -0600

    Added toPaymentPlan test

--- tests/LoanTests.elm
+++ tests/LoanTests.elm
@@ -4,7 +4,7 @@ import Expect exposing (Expectation, FloatingPointTolerance(..))
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
 
-import Loan exposing (getMinimumPaymentAmount)
+import Loan exposing (..)
 
 suite : Test
 suite = 
@@ -23,4 +23,16 @@ suite =
                 in
                 getMinimumPaymentAmount principal apr yearsToPayoff
                     |> Expect.within (Absolute 0.0001) expectedValue
+        , describe "toPaymentPlan"
+            [ test "minimum is sufficient for payment period" <|
+                \_ -> 
+                    let
+                        minimumPayment = 20.0
+                        loan = Loan "Test Loan" 1.0 minimumPayment 200.0
+                        yearsToPayoff = 10
+                        expectedPaymentSequence = [PaymentSequence loan minimumPayment [] False]
+                    in
+                    toPaymentPlan yearsToPayoff [loan] 
+                        |> Expect.equal expectedPaymentSequence
+            ]
         ]
